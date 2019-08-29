2c9869123838398f7de413d1aa19f9ebbfae5fe0
Alfonso Socarras <asocarras10@gmail.com>
2019-08-29 15:12:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -81,13 +81,13 @@ def reduce_to_all_true(source_array)
   while i < source_array.length do
     
     if source_array[i] == source_array[i] 
-      return true 
+      return false 
     end
     i += 1
     
   end
   
- return false 
+ return true 
 end
   
 
